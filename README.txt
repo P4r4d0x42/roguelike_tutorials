3940bd9970418d1b16f808776e1a454ee474e8ff
Paradox <paradox@M1-Studio.home>
2021-10-08 01:54:59 -0700

    Turns out the bulk of part 2 was missing. Not sure what happened there. Here is part 2. Need to debug it though, running into issues with not implemented coming up. Must have missed something.

---
+++ README.txt
@@ -0,0 +1,3 @@
+Docs:
+https://www.reddit.com/r/roguelikedev/wiki/python_tutorial_series#wiki_directory_of_participants
+
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    map_width = 80
+    map_height = 50
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 0, 0))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    game_map = GameMap(map_width, map_height)
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     event_handler = EventHandler()
 
@@ -26,25 +38,13 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print_(x=player_x, y=player_y, string="@")
+            engine.render(console=root_console, context=context)
+
+            events = tcod.event.wait()
 
             context.present(root_console)
 
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
-
+            engine.handle_events(events)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
